8c9cd07c04da2ec13d0e8084f8fcfc3f210c17d7
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2026-07-24 22:53:08 -0500

    feat(buttons): add step-by-step scan controls and reorganize button layout

--- src/app/components/buttons/buttons.ts
+++ src/app/components/buttons/buttons.ts
@@ -4,21 +4,28 @@ import {HumanIcon} from '@app/components/icons/human-icon/human-icon';
 import {MicroscopeIcon} from '@app/components/icons/microscope-icon/microscope-icon';
 import {ShuffleIcon} from '@app/components/icons/shuffle-icon/shuffle-icon';
 import {PencilIcon} from '@app/components/icons/pencil-icon/pencil-icon';
+import {DnaIcon} from '@app/components/icons/dna-icon/dna-icon';
 
 @Component({
   selector: 'app-buttons',
-  imports: [MutantIcon, HumanIcon, MicroscopeIcon, ShuffleIcon, PencilIcon],
+  imports: [MutantIcon, HumanIcon, MicroscopeIcon, ShuffleIcon, PencilIcon, DnaIcon],
   templateUrl: './buttons.html',
   styleUrl: './buttons.css'
 })
 export class Buttons {
   editing = input<boolean>(false);
+  stepping = input<boolean>(false);
+  stepPlaying = input<boolean>(false);
+  stepFinished = input<boolean>(false);
 
   loadMutant = output<void>();
   loadHuman = output<void>();
   randomize = output<void>();
   validate = output<void>();
   edit = output<void>();
+  stepThrough = output<void>();
+  togglePlayback = output<void>();
+  nextStep = output<void>();
 
   protected onLoadMutant(): void {
     this.loadMutant.emit();
@@ -38,5 +45,17 @@ export class Buttons {
 
   protected onEdit(): void {
     this.edit.emit();
+  }
+
+  protected onStepThrough(): void {
+    this.stepThrough.emit();
+  }
+
+  protected onTogglePlayback(): void {
+    this.togglePlayback.emit();
+  }
+
+  protected onNextStep(): void {
+    this.nextStep.emit();
   }
 }
