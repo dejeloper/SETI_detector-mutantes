843b07639b0cc96908f3c53837c5a0ee5ca4e874
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2026-07-24 20:12:08 -0500

    feat(app): add app header with DNA icon and centered layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {Board} from './components/board/board';
+import {DnaIcon} from './components/icons/dna-icon/dna-icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Board],
+  imports: [RouterOutlet, Board, DnaIcon],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App { }
+export class App {}
